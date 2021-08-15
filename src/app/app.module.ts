b9befea5662ef7f6e741c8085341c53a73de5416
unknown <nourelhoudataroudi@gmail.com>
2021-08-15 18:30:08 +0200

    [dev] : Added router outlet + Added Http modules and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientService } from './core/_services/client/client.service';
+import { CreditService } from './core/_services/credit/credit.service';
+import { DocumentService } from './core/_services/document/document.service';
+import { ClientComponent } from './views/client/client.component';
+import { CreditComponent } from './views/credit/credit.component';
+import { DocumentComponent } from './views/document/document.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    CreditComponent,
+    DocumentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ClientService,
+    CreditService,
+    DocumentService
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
