1a32e71ac7bbaf77d4da551205c5e8026260d182
unknown <nourelhoudataroudi@gmail.com>
2021-08-15 18:28:28 +0200

    [dev] : Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientComponent } from './views/client/client.component';
+import { CreditComponent } from './views/credit/credit.component';
+import { DocumentComponent } from './views/document/document.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:ClientComponent
+  },
+  {
+    path:'applyForCredit',
+    component:CreditComponent
+  },
+  {
+    path:'uploadDocument',
+    component:DocumentComponent
+  }
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
