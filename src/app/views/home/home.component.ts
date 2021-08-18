e8e6c60c8814004a86d8a2a0c3005cfa01b84d31
unknown <nourelhoudataroudi@gmail.com>
2021-08-18 18:56:05 +0200

    [dev] : Added document form

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -11,22 +11,29 @@ import { Client } from 'src/app/core/_services/_models/client.model';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  imageSrc:any;
   clientForm = new FormGroup({
     CIN_Number : new FormControl(null,[Validators.required]),
     firstName: new FormControl(null,[Validators.required]),
     lastName: new FormControl(null, [Validators.required]),
-    email : new FormControl(null,[Validators.required]),
+    email : new FormControl(null,[Validators.required,Validators.email]),
     phone: new FormControl(null,[Validators.required]),
     age: new FormControl(null,[Validators.required]),
     salary: new FormControl(null,[Validators.required]),
     address: new FormControl(null,[Validators.required]),
-    job: new FormControl(null,[Validators.required]),
-    accountBalance: new FormControl(null,[Validators.required])
+    job: new FormControl(null,[Validators.required])
   });
   creditForm = new FormGroup({
     amount : new FormControl(null,[Validators.required]),
     monthly : new FormControl(null,[Validators.required])
   });
+  documentForm = new FormGroup({
+    CIN_front : new FormControl(null,[Validators.required]),
+    CIN_back : new FormControl(null,[Validators.required]),
+    salaryCertificat : new FormControl(null,[Validators.required]),
+    certificateResWaterElec : new FormControl(null,[Validators.required]),
+    jobCertificate : new FormControl(null,[Validators.required])
+  })
 
   constructor(private clientService:ClientService,
     private router:Router,
@@ -48,7 +55,6 @@ export class HomeComponent implements OnInit {
       // err=>{
       //     console.log(err)
       //   })
-      console.log(this.clientForm.value)
   }
   createCredit(){
   //   this.creditService.createCredit(this.creditForm.value,this.CIN)
@@ -59,7 +65,50 @@ export class HomeComponent implements OnInit {
   //   err=>{
   //     console.log(err)
     // })
-    console.log(this.creditForm.value);
+  }
+  // Check if one of form control has an error
+	isClientControlHasError(controlName: string, validationType: string): boolean {
+		const control = this.clientForm.controls[controlName];
+		if (!control) {
+			return false;
+		}
+		return control.hasError(validationType) && (control.dirty || control.touched);
+	}
+  isCreditControlHasError(controlName: string, validationType: string): boolean {
+		const control = this.creditForm.controls[controlName];
+		if (!control) {
+			return false;
+		}
+		return control.hasError(validationType) && (control.dirty || control.touched);
+	}
+  onFileChange(event:any,formControl:string) {
+    // const reader = new FileReader();
+    
+    // if(event.target.files && event.target.files.length) {
+    //   const [file] = event.target.files;
+    //   reader.readAsDataURL(file);
+    
+    //   reader.onload = () => {
+   
+    //     this.imageSrc = reader.result as string;
+    if (event.target.files.length === 0)
+      return;
+ 
+    var mimeType = event.target.files[0].type;
+      const selectedImg=<File>event.target.files[0]
+      this.documentForm.patchValue({
+        formControl:selectedImg
+      });
+      var reader = new FileReader();
+      // this.imageSrc = event.target.files; 
+      reader.readAsDataURL(event.target.files[0]); 
+      reader.onload = (_event) => { 
+        this.imageSrc=reader.result;
+
+      }
+  }
+  uploadDocument(){
+    console.log(this.documentForm.value);
   }
 
 }
