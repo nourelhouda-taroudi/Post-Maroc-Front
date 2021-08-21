9dc6cdb8291454c69958ecb18130d7c13d0fc48d
Nour Elhouda Taroudi <83907681+nourelhouda-taroudi@users.noreply.github.com>
2021-08-21 13:24:33 +0100

    [dev] : Added FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule , ReactiveFormsModule} from "@angular/forms";
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -26,6 +26,7 @@ import { NavbarComponent } from './views/partials/navbar/navbar.component';
     AppRoutingModule,
     HttpClientModule,
 	  ReactiveFormsModule,
+    FormsModule
 	
   ],
   providers: [
