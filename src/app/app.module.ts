03e45aac9d7cdfd7fb39b892ad4326ebbcfa5a82
Nour Elhouda Taroudi <83907681+nourelhouda-taroudi@users.noreply.github.com>
2021-08-21 13:03:09 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -26,7 +26,6 @@ import { NavbarComponent } from './views/partials/navbar/navbar.component';
     AppRoutingModule,
     HttpClientModule,
 	  ReactiveFormsModule,
-    FormsModule
 	
   ],
   providers: [
