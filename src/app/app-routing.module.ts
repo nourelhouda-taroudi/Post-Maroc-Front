55d2e2561c0cfdbda7f339bc3d68c7980639b7a2
Nour Elhouda Taroudi <83907681+nourelhouda-taroudi@users.noreply.github.com>
2021-08-22 11:36:57 +0100

    [dev] : Added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,39 @@
-import { HomeComponent } from './views/home/home.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClientService } from './core/_services/client/client.service';
+import { CreditService } from './core/_services/credit/credit.service';
+import { DocumentService } from './core/_services/document/document.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './views/home/home.component';
 import { DocumentValidationComponent } from './views/document-validation/document-validation.component';
-import { AccessGuard } from './core/_guards/access.guard';
-
-const routes: Routes = [
-  {
-    path:'',
-    component:HomeComponent
-  },
-  {
-    path:'document-validation',
-    component:DocumentValidationComponent,
-    canActivate: [AccessGuard]
-  },
-  {
-    path:'**',
-    redirectTo :'/' // TODO : Create 404 error page
-  }
-
-];
+import { NavbarComponent } from './views/partials/navbar/navbar.component';
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    DocumentValidationComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+	  ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [
+    ClientService,
+    CreditService,
+    DocumentService
+
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppRoutingModule { }
+export class AppModule { }
