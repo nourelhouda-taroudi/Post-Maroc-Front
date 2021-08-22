8705f0ab99737f0e7141c54dbc8aab5ce1675742
Nour Elhouda Taroudi <83907681+nourelhouda-taroudi@users.noreply.github.com>
2021-08-22 11:46:07 +0100

    Update app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -9,68 +9,68 @@ $(document).ready(function(){
     
     $(".next").click(function(){
     
-    current_fs = $(this).parent();
-    next_fs = $(this).parent().next();
-    
-    //Add Class Active
-    $("#progressbar li").eq($("form").index(next_fs)).addClass("active");
-    
-    //show the next form
-    next_fs.show();
-    //hide the current form with style
-    current_fs.animate({opacity: 0}, {
-    step: function(now) {
-    // for making fielset appear animation
-    opacity = 1 - now;
-    
-    current_fs.css({
-    'display': 'none',
-    'position': 'relative'
-    });
-    next_fs.css({'opacity': opacity});
-    },
-    duration: 500
-    });
-    setProgressBar(++current);
+        current_fs = $(this).parent();
+        next_fs = $(this).parent().next();
+        
+        //Add Class Active
+        $("#progressbar li").eq($("form").index(next_fs)).addClass("active");
+        
+        //show the next form
+        next_fs.show();
+        //hide the current form with style
+        current_fs.animate({opacity: 0}, {
+            step: function(now) {
+                // for making fielset appear animation
+                opacity = 1 - now;
+                
+                current_fs.css({
+                'display': 'none',
+                'position': 'relative'
+                });
+                next_fs.css({'opacity': opacity});
+            },
+            duration: 500
+            });
+        setProgressBar(++current);
     });
     
     $(".previous").click(function(){
     
-    current_fs = $(this).parent();
-    previous_fs = $(this).parent().prev();
-    
-    //Remove class active
-    $("#progressbar li").eq($("form").index(current_fs)).removeClass("active");
-    
-    //show the previous form
-    previous_fs.show();
-    
-    //hide the current form with style
-    current_fs.animate({opacity: 0}, {
-    step: function(now) {
-    // for making fielset appear animation
-    opacity = 1 - now;
-    
-    current_fs.css({
-    'display': 'none',
-    'position': 'relative'
-    });
-    previous_fs.css({'opacity': opacity});
-    },
-    duration: 500
-    });
-    setProgressBar(--current);
+        current_fs = $(this).parent();
+        previous_fs = $(this).parent().prev();
+        
+        //Remove class active
+        $("#progressbar li").eq($("form").index(current_fs)).removeClass("active");
+        
+        //show the previous form
+        previous_fs.show();
+        
+        //hide the current form with style
+        current_fs.animate({opacity: 0}, {
+            step: function(now) {
+                // for making fielset appear animation
+                opacity = 1 - now;
+                
+                current_fs.css({
+                    'display': 'none',
+                    'position': 'relative'
+                });
+                previous_fs.css({'opacity': opacity});
+            },
+            duration: 500
+        });
+        setProgressBar(--current);
     });
     
     function setProgressBar(curStep){
-    var percent = parseFloat(100 / steps) * curStep;
-    percent = percent.toFixed();
-    $(".progress-bar")
-    .css("width",percent+"%")
+        var percent = parseFloat(100 / steps) * curStep;
+        percent = percent.toFixed();
+        $(".progress-bar")
+        .css("width",percent+"%")
     }
     
     $(".submit").click(function(){
-    return false;
+        return false;
     })
 
 });
