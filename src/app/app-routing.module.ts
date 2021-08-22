71e82ac15d036395729129b8dd1f37c63a8780ea
Nour Elhouda Taroudi <83907681+nourelhouda-taroudi@users.noreply.github.com>
2021-08-22 11:37:59 +0100

    [dev] : Fixed push error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ClientService } from './core/_services/client/client.service';
-import { CreditService } from './core/_services/credit/credit.service';
-import { DocumentService } from './core/_services/document/document.service';
-import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './views/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { DocumentValidationComponent } from './views/document-validation/document-validation.component';
-import { NavbarComponent } from './views/partials/navbar/navbar.component';
+import { AccessGuard } from './core/_guards/access.guard';
+
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'document-validation',
+    component:DocumentValidationComponent,
+    canActivate: [AccessGuard]
+  },
+  {
+    path:'**',
+    redirectTo :'/' // TODO : Create 404 error page
+  }
+
+];
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    DocumentValidationComponent,
-    NavbarComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-	  ReactiveFormsModule,
-    FormsModule
-  ],
-  providers: [
-    ClientService,
-    CreditService,
-    DocumentService
-
-  ],
-  bootstrap: [AppComponent]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppModule { }
+export class AppRoutingModule { }
