7cc7af92ab20ad08b43b224a430c854b25918ce3
Nour Elhouda Taroudi <83907681+nourelhouda-taroudi@users.noreply.github.com>
2021-08-22 11:42:21 +0100

    Update document.service.ts

--- src/app/core/_services/document/document.service.ts
+++ src/app/core/_services/document/document.service.ts
@@ -1,9 +1,26 @@
+import { environment } from './../../../../environments/environment';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+
+const API_DOC = environment.API_URL;
 @Injectable({
   providedIn: 'root'
 })
 export class DocumentService {
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
+  
+  uploadDocument(payload:any,CIN:string):Observable<any>{
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'multipart/form-data');
+    headers.append('Accept', 'application/json');
+    return this.http.post(`${API_DOC}/${CIN}/uploadDocument`,payload,{headers: headers});
+  }
+  signer(idCredit:number,pdf:string):Observable<any>{
+    return this.http.put(`${API_DOC}/${idCredit}/sign`,pdf);
+  }
 }
