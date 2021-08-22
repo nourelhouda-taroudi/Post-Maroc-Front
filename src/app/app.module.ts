724579cfdf9aaa3285c04b944a496f9c01a39616
Nour Elhouda Taroudi <83907681+nourelhouda-taroudi@users.noreply.github.com>
2021-08-22 11:39:16 +0100

    [dev] : Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,27 +8,25 @@ import { AppComponent } from './app.component';
 import { ClientService } from './core/_services/client/client.service';
 import { CreditService } from './core/_services/credit/credit.service';
 import { DocumentService } from './core/_services/document/document.service';
-import { ClientComponent } from './views/client/client.component';
-import { CreditComponent } from './views/credit/credit.component';
-import { DocumentComponent } from './views/document/document.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './views/home/home.component';
+import { DocumentValidationComponent } from './views/document-validation/document-validation.component';
+import { NavbarComponent } from './views/partials/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientComponent,
-    CreditComponent,
-    DocumentComponent,
     HomeComponent,
+    DocumentValidationComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
 	  ReactiveFormsModule,
-	
+    FormsModule
   ],
   providers: [
     ClientService,
